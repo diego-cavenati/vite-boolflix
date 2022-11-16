038bb3b7e2a4d7045f2a607bae49cfac26513ee7
diego-cavenati <diegocavenati@gmail.com>
2022-11-16 15:50:52 +0100

    add call api

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,47 @@
 <script>
+import { store } from './store.js';
+import axios from 'axios';
+// import AppHeader from './components/AppHeader.vue';
+// import AppMain from './components/AppMain.vue';
+// import AppFooter from './components/AppFooter.vue';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      store,
+    }
+  },
+  methods: {
+    callApi(url) {
+      axios
+        .get(url)
+        .then(response => {
+          console.log(response);
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    }
+  },
+  mounted() {
+    this.callApi('https://api.themoviedb.org/3/search/movie?api_key=e14460b68e70bbc26e66a03289f00772&language=en-US&query=thor&page=1&include_adult=false')
+  },
+  // components: {
+  //   AppHeader,
+  //   AppMain,
+  //   AppFooter,
+  // },
 }
 </script>
 
 <template>
-  <h1>Ciao</h1>
+
+
+  <!-- <AppHeader />
+  <AppMain />
+  <AppFooter /> -->
+
 </template>
 
 <style>
