0668d4abb640bacf0e248d7064b34a2bf6fd9e96
diego-cavenati <diegocavenati@gmail.com>
2022-11-17 13:35:36 +0100

    Add milestone 2 and fix axios call

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -6,6 +6,16 @@ export default {
         return {
             store,
         }
+    },
+    methods: {
+        // flag() {
+        //     if (store.original_language != null) {
+        //         if (store.original_language == 'en') {
+        //             store.original_language = 'gb'
+        //         }
+        //         return store.original_language
+        //     }
+        // }
     }
 }
 </script>
@@ -15,11 +25,18 @@ export default {
     <ul v-if="store.results">
         <!-- <li v-for="movie in store.movies"> -->
         <li v-for="movie in store.results.results">
-            {{ movie.title }}
-            {{ movie.original_title }}
-            {{ movie.original_language }}
-            {{ movie.vote_average }}
-
+            <div class="movieSearch" v-if="movie.media_type == 'movie'">
+                {{ movie.title }}
+                {{ movie.original_title }}
+                <img :src="`https://countryflagsapi.com/png/${movie.original_language}`" alt="">
+                {{ movie.vote_average }}
+            </div>
+            <div class="tvShow" v-else>
+                {{ movie.name }}
+                {{ movie.original_title }}
+                <img :src="`https://countryflagsapi.com/png/${movie.original_language}`" alt="">
+                {{ movie.vote_average }}
+            </div>
         </li>
     </ul>
 
--- src/store.js
+++ src/store.js
@@ -11,11 +11,10 @@ export const store = reactive({
     title: null,
     vote_average: null,
     errorMessage: null,
-    // API_url: 'https://api.themoviedb.org/3/search/movie?api_key=e14460b68e70bbc26e66a03289f00772&query=thor',
 
     config: {
         method: 'get',
-        url: 'https://api.themoviedb.org/3/search/movie',
+        url: 'https://api.themoviedb.org/3/search/multi',
         params: {
             api_key: 'e14460b68e70bbc26e66a03289f00772',
             query: '',
@@ -24,14 +23,28 @@ export const store = reactive({
     results: null,
     error: false,
     callApi() {
-        axios
-            .get(store.config)
+        axios(store.config)
             .then(response => {
-                console.log(response);
                 // this.store.movies = response.data.results;
                 store.results = response.data;
                 store.loading = false;
-                store.config.params.query = ''
+                store.config.params.query = '';
+
+                console.log(response.data.results[0].original_language);
+                response.data.results.forEach(element => {
+                    console.log(element);
+                    if (element.original_language != null) {
+                        if (element.original_language == 'en') {
+                            element.original_language = 'gb'
+                        }
+                        return element.original_language
+                    }
+                });
+                console.log(element.original_language);
+
+
+
+
             })
             .catch(err => {
                 console.log(err);
@@ -39,6 +52,7 @@ export const store = reactive({
                 store.error = err.message
             })
     },
+
 
 
 
