afcbbab56eda594a2e226f8230fce48f71961b5e
diego-cavenati <diegocavenati@gmail.com>
2022-11-18 18:21:33 +0100

    Add header style

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -14,24 +14,43 @@ export default {
 <template>
     <header>
         <div class="logo">
-            Logo
+            <img src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="">
         </div>
 
         <div class="search-box">
             <!-- @keyup="$emit('performSearch')" -->
-            <input type="search" v-model="store.config.params.query">
+            <input type="search" v-model="store.config.params.query" placeholder="cerca un film">
             <button @click="$emit('performSearch')">Search</button>
         </div>
     </header>
 </template>
 
-<style>
+<style lang="scss" scoped>
 header {
     padding-top: 2rem;
     text-align: center;
 }
 
-input {}
+.logo {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    img {
+        width: 10%;
+        text-align: center;
+        margin: 0 auto;
+    }
+}
+
+input {
+    min-width: 20%;
+    font-size: 1.2rem;
+    padding: 1rem;
+    border-radius: 0.5rem;
+    border: none;
+}
 
 button {
     background-color: red;
